2512d55cc8cbe32a6e710e16d40fc4b873e96ced
xciter327 <xciter327@gmail.com>
2023-04-15 22:51:06 +0200

    Adhoc: Make NextDNS also use global config file

--- nextdns/install-next-dns.sh
+++ nextdns/install-next-dns.sh
@@ -1,10 +1,17 @@
 #/bin/bash
+set e
+# path to config file. One level up by default
+CONFIG_FILE="$PWD../config.json"
+
 # NextDNS Token
-NEXTDNS_TOKEN='XXXXXXXXXXX'
+NEXTDNS_TOKEN="jq -r nextdns-token $CONFIG_FILE"
+
 # Add repo
 curl -Ls https://repo.nextdns.io/nextdns.repo -o /etc/yum.repos.d/nextdns.repo
+
 # Install NextDNS
 yum install -y nextdns
+
 # Setup NextDNS binary in router mode
 nextdns install \
   --config "$NEXTDNS_TOKEN" \
